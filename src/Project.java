e6ab64b7e6b5874c6824aa8fed770667c99ab207
Maxim Builin <builin.maxim@gmail.com>
2014-10-12 13:39:23 +0400

    project initial

---
+++ src/Project.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: MakcuK
+ * Date: 12.10.14
+ * Time: 13:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Project {
+}
